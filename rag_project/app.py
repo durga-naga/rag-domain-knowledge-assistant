575fc75b254fbc7790d29291ef8d07941ffb5d77
durga-naga <gangulanagadurga@gmail.com>
2026-08-18 11:35:56 +0530

    Change model from llama-3.1-8b-instant to openai/gpt-oss-20b

--- rag_project/app.py
+++ rag_project/app.py
@@ -14,7 +14,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def get_answer(context, question):
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-20b",
         messages=[
     {
     "role": "system",
